97f6053aa516be5b6502cbf65e2cf067366fe2dd
naoki_sakata <naoki.sakata@hopin.co.jp>
2024-07-25 17:33:33 +0900

    add totalMinutes -> Like

--- src/services/like.service.ts
+++ src/services/like.service.ts
@@ -4,36 +4,41 @@ import {
   GqlMutationAddLikeArgs,
   GqlMutationRemoveLikeArgs,
 } from "@/types/graphql";
-import { Prisma } from "@prisma/client";
 
 export default class LikeService {
   private static db = prismaClient;
 
   static async addLike({ content }: GqlMutationAddLikeArgs): Promise<GqlLike> {
-    const data: Prisma.LikeCreateInput = {
-      event: {
-        connect: { id: content.eventId },
+    const like = await this.db.like.create({
+      data: {
+        event: { connect: { id: content.eventId } },
+        user: { connect: { id: content.userId } },
+        postedAt: new Date(content.postedAt ?? Date.now()).toISOString(),
       },
-      user: {
-        connect: { id: content.userId },
-      },
-      postedAt: new Date(content.postedAt ?? Date.now()).toISOString(),
-    };
-
-    return this.db.like.create({
-      data,
       include: {
         user: true,
-        event: true,
+        event: {
+          include: {
+            stat: { select: { totalMinutes: true } },
+          },
+        },
       },
     });
+
+    return {
+      ...like,
+      event: {
+        ...like.event,
+        totalMinutes: like.event.stat?.totalMinutes ?? 0,
+      },
+    };
   }
 
   static async removeLike({
     eventId,
     userId,
   }: GqlMutationRemoveLikeArgs): Promise<GqlLike> {
-    return this.db.like.delete({
+    const like = await this.db.like.delete({
       where: {
         userId_eventId: {
           eventId,
@@ -42,8 +47,20 @@ export default class LikeService {
       },
       include: {
         user: true,
-        event: true,
+        event: {
+          include: {
+            stat: { select: { totalMinutes: true } },
+          },
+        },
       },
     });
+
+    return {
+      ...like,
+      event: {
+        ...like.event,
+        totalMinutes: like.event.stat?.totalMinutes ?? 0,
+      },
+    };
   }
 }
