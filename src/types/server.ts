8d118309b702f8481afd1ad8ba62bb45aed46d85
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-02-11 10:05:22 +0900

    add type

--- src/types/server.ts
+++ src/types/server.ts
@@ -1,9 +1,10 @@
 import { IdentityPlatform } from "@prisma/client";
 import { AuthGetPayloadWithArgs } from "@/domains/user/type";
 
-type LoggedInUserInfo = {
+export type LoggedInUserInfo = {
   uid: string;
   platform: IdentityPlatform;
   currentUser: AuthGetPayloadWithArgs | null;
 };
+
 export type IContext = Record<string, never> | LoggedInUserInfo;
