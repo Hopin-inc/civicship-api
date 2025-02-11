dc2f194cf9ce9a97c5b140f0ebb325f0656cc7b9
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-02-11 10:04:36 +0900

    create pagination

--- src/graphql/pagination.ts
+++ src/graphql/pagination.ts
@@ -1,6 +1,13 @@
+import { GraphQLError } from "graphql/error";
+
 export function clampFirst(first: number | null | undefined): number {
   if (typeof first === "number" && first > 100) {
-    throw new Error("Too many items requested. Maximum is 100.");
+    throw new GraphQLError(`Cannot request more than ${first} objects`, {
+      extensions: {
+        code: "LIMIT_EXCEEDED",
+        first,
+      },
+    });
   }
 
   return first ?? 10;
