587f54dadf0e6ea1bc670b572a42db631c00bceb
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-02-13 20:38:02 +0900

    modify prismaClient -> issuer

--- src/domains/opportunity/participation/service.ts
+++ src/domains/opportunity/participation/service.ts
@@ -1,17 +1,23 @@
 import {
   GqlParticipationApplyInput,
   GqlParticipationInviteInput,
+  GqlParticipationSetStatusInput,
   GqlQueryParticipationsArgs,
 } from "@/types/graphql";
 import { ParticipationStatus, Prisma } from "@prisma/client";
 import ParticipationInputFormat from "@/domains/opportunity/participation/presenter/input";
 import ParticipationRepository from "@/domains/opportunity/participation/repository";
-import { prismaClient } from "@/prisma/client";
+import { PrismaClientIssuer } from "@/prisma/client";
 import OpportunityRepository from "@/domains/opportunity/repository";
 import { IContext } from "@/types/server";
-import { ParticipationUtils } from "@/domains/opportunity/participation/utils";
+import ParticipationUtils from "@/domains/opportunity/participation/utils";
+import { getCurrentUserId } from "@/utils";
+import MembershipUtils from "@/domains/membership/utils";
+import ParticipationStatusHistoryService from "@/domains/opportunity/participationStatusHistory/service";
 
 export default class ParticipationService {
+  private static issuer = new PrismaClientIssuer();
+
   static async fetchParticipations(
     ctx: IContext,
     { cursor, filter, sort }: GqlQueryParticipationsArgs,
@@ -28,43 +34,26 @@ export default class ParticipationService {
   }
 
   static async inviteParticipation(ctx: IContext, input: GqlParticipationInviteInput) {
-    const currentUserId = ctx.currentUser?.id;
-    if (!currentUserId) {
-      throw new Error("Unauthorized: User must be logged in");
-    }
+    const userId = getCurrentUserId(ctx);
 
-    return await prismaClient.$transaction(async (tx) => {
-      const opportunity = await OpportunityRepository.findWithTransaction(
+    return this.issuer.public(ctx, async (tx) => {
+      const data: Prisma.ParticipationCreateInput = ParticipationInputFormat.invite(input);
+
+      const participation = await ParticipationRepository.create(
         ctx,
+        {
+          ...data,
+          status: ParticipationStatus.INVITED,
+        },
         tx,
-        input.opportunityId,
       );
 
-      if (!opportunity) {
-        throw new Error(`OpportunityNotFound: ID=${input.opportunityId}`);
-      }
-
-      const communityId = opportunity.community.id;
-      if (!communityId) {
-        throw new Error(`CommunityNotFound: ID=${opportunity.community}`);
-      }
-
-      const data: Prisma.ParticipationCreateInput = ParticipationInputFormat.invite(
-        input,
-        communityId,
-      );
-
-      const participation = await ParticipationRepository.createWithTransaction(ctx, tx, {
-        ...data,
-        status: ParticipationStatus.INVITED,
-      });
-
-      await ParticipationUtils.recordParticipationHistory(
+      await ParticipationStatusHistoryService.recordParticipationHistory(
         ctx,
         tx,
         participation.id,
         ParticipationStatus.INVITED,
-        currentUserId,
+        userId,
       );
 
       return participation;
@@ -72,56 +61,65 @@ export default class ParticipationService {
   }
 
   static async applyParticipation(ctx: IContext, input: GqlParticipationApplyInput) {
-    const currentUserId = ctx.currentUser?.id;
-    if (!currentUserId) {
-      throw new Error("Unauthorized: User must be logged in");
-    }
+    const userId = getCurrentUserId(ctx);
 
-    return await prismaClient.$transaction(async (tx) => {
-      const opportunity = await OpportunityRepository.findWithTransaction(
-        ctx,
-        tx,
-        input.opportunityId,
-      );
-
+    return this.issuer.public(ctx, async (tx) => {
+      const opportunity = await OpportunityRepository.find(ctx, input.opportunityId, tx);
       if (!opportunity) {
         throw new Error(`OpportunityNotFound: ID=${input.opportunityId}`);
       }
 
-      const communityId = opportunity.community.id;
-      if (!communityId) {
-        throw new Error(`CommunityNotFound: ID=${opportunity.community}`);
-      }
-
       const data: Prisma.ParticipationCreateInput = ParticipationInputFormat.apply(
         input,
-        currentUserId,
-        communityId,
+        userId,
+        opportunity.community.id,
       );
 
       const participationStatus = opportunity.requireApproval
         ? ParticipationStatus.APPLIED
         : ParticipationStatus.PARTICIPATING;
 
-      const participation = await ParticipationRepository.createWithTransaction(ctx, tx, {
-        ...data,
-        status: participationStatus,
-      });
+      if (participationStatus === ParticipationStatus.PARTICIPATING) {
+        await MembershipUtils.joinCommunityAndCreateMemberWallet(
+          ctx,
+          tx,
+          userId,
+          opportunity.community.id,
+        );
+      }
 
-      await ParticipationUtils.recordParticipationHistory(
+      const participation = await ParticipationRepository.create(
+        ctx,
+        {
+          ...data,
+          status: participationStatus,
+        },
+        tx,
+      );
+
+      await ParticipationStatusHistoryService.recordParticipationHistory(
         ctx,
         tx,
         participation.id,
         participationStatus,
-        currentUserId,
+        userId,
       );
 
       return participation;
     });
   }
 
-  static async cancelInvitation(ctx: IContext, id: string) {
-    return ParticipationUtils.setParticipationStatus(ctx, id, ParticipationStatus.CANCELED);
+  static async cancelInvitation(
+    ctx: IContext,
+    id: string,
+    { communityId }: GqlParticipationSetStatusInput,
+  ) {
+    return ParticipationUtils.setParticipationStatus(
+      ctx,
+      id,
+      ParticipationStatus.CANCELED,
+      communityId,
+    );
   }
 
   static async acceptInvitation(ctx: IContext, id: string) {
@@ -140,23 +138,55 @@ export default class ParticipationService {
     return ParticipationUtils.setParticipationStatus(ctx, id, ParticipationStatus.CANCELED);
   }
 
-  static async acceptApplication(ctx: IContext, id: string) {
-    return ParticipationUtils.setParticipationStatus(ctx, id, ParticipationStatus.PARTICIPATING);
+  static async acceptApplication(
+    ctx: IContext,
+    id: string,
+    { communityId }: GqlParticipationSetStatusInput,
+  ) {
+    return ParticipationUtils.setParticipationStatus(
+      ctx,
+      id,
+      ParticipationStatus.PARTICIPATING,
+      communityId,
+    );
   }
 
-  static async denyApplication(ctx: IContext, id: string) {
+  static async denyApplication(
+    ctx: IContext,
+    id: string,
+    { communityId }: GqlParticipationSetStatusInput,
+  ) {
     return ParticipationUtils.setParticipationStatus(
       ctx,
       id,
       ParticipationStatus.NOT_PARTICIPATING,
+      communityId,
     );
   }
 
-  static async approvePerformance(ctx: IContext, id: string) {
-    return ParticipationUtils.setParticipationStatus(ctx, id, ParticipationStatus.APPROVED, true);
+  static async approvePerformance(
+    ctx: IContext,
+    id: string,
+    { communityId }: GqlParticipationSetStatusInput,
+  ) {
+    return ParticipationUtils.setParticipationStatus(
+      ctx,
+      id,
+      ParticipationStatus.APPROVED,
+      communityId,
+    );
   }
 
-  static async denyPerformance(ctx: IContext, id: string) {
-    return ParticipationUtils.setParticipationStatus(ctx, id, ParticipationStatus.DENIED);
+  static async denyPerformance(
+    ctx: IContext,
+    id: string,
+    { communityId }: GqlParticipationSetStatusInput,
+  ) {
+    return ParticipationUtils.setParticipationStatus(
+      ctx,
+      id,
+      ParticipationStatus.DENIED,
+      communityId,
+    );
   }
 }
