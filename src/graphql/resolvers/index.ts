494a22eaa97f6f037cec915d9bd987ebc386e9eb
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-02-15 14:46:15 +0900

    feature: create decimal resolver

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -2,14 +2,19 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers } from "@graphql-tools/merge";
 import path from "path";
 import { fileURLToPath } from "url";
+import { DecimalScalar } from "@/graphql/resolvers/place/decimal";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const scalarResolvers = {
+  Decimal: DecimalScalar,
+};
+
 const resolversArray = loadFilesSync([
-  `${__dirname}/*.resolver.ts`,
-  `${__dirname}/../*.resolver.ts`,
+  path.join(__dirname, "resolvers", "**", "*.ts"),
+  "!" + path.join(__dirname, "resolvers", "index.ts"),
 ]);
-const resolvers = mergeResolvers(resolversArray);
+const resolvers = mergeResolvers([scalarResolvers, ...resolversArray]);
 
 export default resolvers;
