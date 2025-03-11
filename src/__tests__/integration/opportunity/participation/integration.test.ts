4d5b77cad2dffe39511cc8f92344ec0be8f0b0ca
Yudai Tatsumi <yudai.tatsumi@hopin.co.jp>
2025-03-12 02:23:52 +0900

    test: add wallet create integration test

--- src/__tests__/integration/opportunity/participation/integration.test.ts
+++ src/__tests__/integration/opportunity/participation/integration.test.ts
@@ -99,4 +99,87 @@ describe("Participation Integration Tests", () => {
         const memberCurrentPointActual = (await TestDataSourceHelper.findMemberWallet(userId))?.currentPointView?.currentPoint;
         expect(memberCurrentPointActual).not.toBeDefined;
     });
+
+    it("should accept participation invitation", async () => {
+        //////////////////////////////////////////////////
+        // insert seed data
+        //////////////////////////////////////////////////
+        const name = "Jane Doe"
+        const slug = "user-2-slug"
+        const createUserInput = {
+            name: name,
+            slug: slug,
+            image: undefined
+        }
+        const userInserted = await TestDataSourceHelper.create(createUserInput);
+        const userId = userInserted.id;
+
+        const ctx = { currentUser: { id: userId } } as unknown as IContext;
+
+        const communityName = "community-2";
+        const pointName = "community-2-point";
+
+        const createCommunityInput = {
+            name: communityName,
+            pointName: pointName,
+            image: undefined,
+            bio: undefined,
+            establishedAt: undefined,
+            website: undefined
+        };
+        const communityInserted = await TestDataSourceHelper.createCommunity(createCommunityInput);
+        const communityId = communityInserted.id;
+
+        const createOpportunityInput = {
+            category: OpportunityCategory.EVENT,
+            description: "new opportunity",
+            publishStatus: PublishStatus.PUBLIC,
+            requireApproval: true,
+            title: "new opportunity",
+            community: { connect: { id: communityId } },
+            createdByUser: { connect: { id: userId } },
+        };
+        const opportunityInserted = await TestDataSourceHelper.createOpportunity(createOpportunityInput);
+        const opportunityId = opportunityInserted.id;
+
+        const createParticipationInput = {
+            status: ParticipationStatus.INVITED,
+            community: { connect: { id: communityId } },
+            user: { connect: { id: userId } },
+            opportunity: { connect: { id: opportunityId } }
+        }
+        const participationInserted = await TestDataSourceHelper.createParticipation(createParticipationInput);
+        const participationId = participationInserted.id;
+
+        //////////////////////////////////////////////////
+        // construct request
+        //////////////////////////////////////////////////
+        //////////////////////////////////////////////////
+        // execute
+        //////////////////////////////////////////////////
+        await participationResolver.Mutation.participationAcceptMyInvitation(
+            {},
+            {
+                id: participationId,
+            },
+            ctx
+        );
+
+        //////////////////////////////////////////////////
+        // assert result
+        //////////////////////////////////////////////////
+        const participationActual = await TestDataSourceHelper.findParticipationById(participationId);
+
+        // 参加ステータスが INVITED から PARTICIPATING に変更されていること
+        expect(participationActual?.status).toEqual(ParticipationStatus.PARTICIPATING);
+
+        // member walletが作成されていること
+        const memberWallet = await TestDataSourceHelper.findMemberWallet(userId);
+        expect(memberWallet).toBeDefined();
+
+        // walletが作られた直後は、mv_current_pointsが作成されていない
+        const memberCurrentPointActual = memberWallet?.currentPointView?.currentPoint;
+        expect(memberCurrentPointActual).not.toBeDefined();
+    });
+
 });
