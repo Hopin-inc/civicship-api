5be5eadf3d7c3b14a7b0d24d038b530e9aea60fa
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2025-03-17 07:28:57 +0900

    fix: fix resolver error causing app not run

--- src/presentation/graphql/resolvers/index.ts
+++ src/presentation/graphql/resolvers/index.ts
@@ -2,16 +2,11 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers } from "@graphql-tools/merge";
 import path from "path";
 import { fileURLToPath } from "url";
-import { DecimalScalar } from "@/presentation/graphql/resolvers/place/decimal";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const scalarResolvers = {
-  Decimal: DecimalScalar,
-};
-
-const resolversArray = loadFilesSync([path.join(__dirname, "resolvers", "**", "*.ts")]);
-const resolvers = mergeResolvers([scalarResolvers, ...resolversArray]);
+const resolversArray = loadFilesSync([path.join(__dirname, "*", "**", "*.ts")]);
+const resolvers = mergeResolvers([...resolversArray]);
 
 export default resolvers;
