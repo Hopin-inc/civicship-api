a0b4f6b91658f70d1ddca9d7e800bff4859fd7d6
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-03-25 01:20:55 +0900

    refactor: update membership history

--- src/application/membership/data/type.ts
+++ src/application/membership/data/type.ts
@@ -2,14 +2,10 @@ import { Prisma } from "@prisma/client";
 import { communityInclude } from "@/application/community/data/type";
 import { userInclude } from "@/application/user/data/type";
 
-export const membershipAuthSelect = Prisma.validator<Prisma.MembershipSelect>()({
-  role: true,
-  communityId: true,
-});
-
 export const membershipInclude = Prisma.validator<Prisma.MembershipInclude>()({
   community: { include: communityInclude },
   user: { include: userInclude },
+  histories: true,
 });
 
 export type PrismaMembership = Prisma.MembershipGetPayload<{
