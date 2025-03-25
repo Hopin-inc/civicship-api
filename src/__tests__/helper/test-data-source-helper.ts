a42c5a8e6d9cada6e521b1a77f219b72c850357f
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-03-26 03:01:22 +0900

    [WIP] enhance: ...

--- src/__tests__/helper/test-data-source-helper.ts
+++ src/__tests__/helper/test-data-source-helper.ts
@@ -1,14 +1,14 @@
 import { prismaClient } from "@/infrastructure/prisma/client";
-import { prismaClient } from "@/infra/prisma/client";
-import { communityInclude } from "@/infra/prisma/types/community";
-import { walletInclude } from "@/infra/prisma/types/membership/wallet";
-import { opportunityInclude } from "@/infra/prisma/types/opportunity";
-import { participationInclude } from "@/infra/prisma/types/opportunity/participation";
-import { placeInclude } from "@/infra/prisma/types/place";
-import { transactionInclude } from "@/infra/prisma/types/transaction";
-import { utilityInclude } from "@/infra/prisma/types/utility";
 import { Prisma, WalletType } from "@prisma/client";
 import { refreshMaterializedViewCurrentPoints } from "@prisma/client/sql";
+import { opportunityInclude } from "@/application/opportunity/data/type";
+import { communityInclude } from "@/application/community/data/type";
+import { walletInclude } from "@/application/membership/wallet/data/type";
+import { transactionInclude } from "@/application/transaction/data/type";
+import { utilityInclude } from "@/application/utility/data/type";
+import { participationInclude } from "@/application/participation/data/type";
+import { placeInclude } from "@/application/place/data/type";
+import { membershipInclude } from "@/application/membership/data/type";
 
 export default class TestDataSourceHelper {
   private static db = prismaClient;
@@ -18,7 +18,8 @@ export default class TestDataSourceHelper {
   }
 
   static async deleteAll() {
-    await this.db.utilityHistory.deleteMany();
+    await this.db.ticket.deleteMany();
+    await this.db.ticketStatusHistory.deleteMany();
     await this.db.transaction.deleteMany();
     await this.db.opportunityInvitationHistory.deleteMany();
     await this.db.participationStatusHistory.deleteMany();
@@ -37,7 +38,6 @@ export default class TestDataSourceHelper {
     await this.db.state.deleteMany();
   }
 
-
   static async disconnect() {
     return await this.db.$disconnect();
   }
@@ -51,36 +51,35 @@ export default class TestDataSourceHelper {
     });
   }
 
-  static async createCommunity(
-    data: Prisma.CommunityCreateInput
-  ) {
+  static async createCommunity(data: Prisma.CommunityCreateInput) {
     return await this.db.community.create({
       data,
       include: communityInclude,
     });
   }
 
-  static async createWallet(
-    data: Prisma.WalletCreateInput,
-  ) {
+  static async createMembership(data: Prisma.MembershipCreateInput) {
+    return this.db.membership.create({
+      data,
+      include: membershipInclude,
+    });
+  }
+
+  static async createWallet(data: Prisma.WalletCreateInput) {
     return await this.db.wallet.create({
       data,
       include: walletInclude,
     });
-  };
+  }
 
-  static async findCommunityWallet(
-    communityId: string,
-  ) {
+  static async findCommunityWallet(communityId: string) {
     return await this.db.wallet.findFirst({
       where: { communityId, type: WalletType.COMMUNITY },
       include: walletInclude,
     });
   }
 
-  static async findMemberWallet(
-    userId: string,
-  ) {
+  static async findMemberWallet(userId: string) {
     return await this.db.wallet.findFirst({
       where: { userId, type: WalletType.MEMBER },
       include: walletInclude,
@@ -89,18 +88,14 @@ export default class TestDataSourceHelper {
 
   static async findAllTransactions() {
     return await this.db.transaction.findMany();
-  };
+  }
 
-
-  static async createTransaction(
-    data: Prisma.TransactionCreateInput,
-  ) {
+  static async createTransaction(data: Prisma.TransactionCreateInput) {
     return await this.db.transaction.create({
       data,
       include: transactionInclude,
     });
-  };
-
+  }
 
   static async refreshCurrentPoints() {
     return await this.db.$queryRawTyped(refreshMaterializedViewCurrentPoints());
@@ -127,7 +122,6 @@ export default class TestDataSourceHelper {
     });
   }
 
-
   static async createPlace(data: Prisma.PlaceCreateInput) {
     return await this.db.place.create({
       data,
@@ -143,12 +137,26 @@ export default class TestDataSourceHelper {
   }
 
   static async findAllParticipation() {
-    return this.db.participation.findMany({
-    });
+    return this.db.participation.findMany({});
   }
 
   static async findAllCommunity() {
-    return this.db.community.findMany({
+    return this.db.community.findMany({});
+  }
+
+  static async findAllMembership() {
+    return this.db.membership.findMany({});
+  }
+
+  static async findMembershipById(userId: string, communityId: string) {
+    return await this.db.membership.findUniqueOrThrow({
+      where: {
+        userId_communityId: {
+          userId,
+          communityId,
+        },
+      },
+      include: membershipInclude,
     });
   }
 }
