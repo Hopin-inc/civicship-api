2284269c67a9a5904f5a150c4a19eba874aae8ba
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-04-06 18:12:31 +0900

    fix: remove joinIfNeeded from userCreateAccount

--- src/application/domain/user/identity/usecase.ts
+++ src/application/domain/user/identity/usecase.ts
@@ -3,14 +3,8 @@ import { GqlCurrentUserPayload, GqlMutationUserSignUpArgs } from "@/types/graphq
 import IdentityConverter from "@/application/domain/user/identity/data/converter";
 import IdentityService from "@/application/domain/user/identity/service";
 import IdentityPresenter from "@/application/domain/user/identity/presenter";
-import { PrismaClientIssuer } from "@/infrastructure/prisma/client";
-import MembershipService from "@/application/domain/membership/service";
-import WalletService from "@/application/domain/membership/wallet/service";
-import { initialCommunityId } from "@/consts/utils";
 
 export default class IdentityUseCase {
-  private static issuer = new PrismaClientIssuer();
-
   static async userViewCurrentAccount(context: IContext): Promise<GqlCurrentUserPayload> {
     return {
       user: context.currentUser,
@@ -24,13 +18,7 @@ export default class IdentityUseCase {
     const data = IdentityConverter.create(args);
     const user = await IdentityService.createUserAndIdentity(data, ctx.uid, ctx.platform);
 
-    const res = await this.issuer.public(ctx, async (tx) => {
-      await MembershipService.joinIfNeeded(ctx, user.id, initialCommunityId, tx);
-      await WalletService.createMemberWalletIfNeeded(ctx, user.id, initialCommunityId, tx);
-      return user;
-    });
-
-    return IdentityPresenter.create(res);
+    return IdentityPresenter.create(user);
   }
 
   static async userDeleteAccount(context: IContext): Promise<GqlCurrentUserPayload> {
