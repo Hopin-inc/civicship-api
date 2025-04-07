673574fb56e9a56eb52716e1f47f96c2b4bc8030
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2025-04-07 22:27:33 +0900

    fix: resolve build error

--- src/presentation/graphql/resolver.ts
+++ src/presentation/graphql/resolver.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const resolversArray = loadFilesSync(path.join(__dirname, "../../application/**/resolver.ts"));
+const resolversArray = loadFilesSync(path.resolve(__dirname, "../../application/**/resolver.ts"));
 const resolvers = mergeResolvers([...resolversArray]);
 
 export default resolvers;
