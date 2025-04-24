6971aab50ac5e2c33bab35a5f776fbae0d61befd
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2025-04-25 08:50:16 +0900

    feat: modify response logging

--- src/presentation/middleware/logger.ts
+++ src/presentation/middleware/logger.ts
@@ -1,11 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import logger from "@/infrastructure/logging";
 
-export const requestLogger = (req: Request, _res: Response, next: NextFunction) => {
-  logger.info(`${req.method} ${req.originalUrl}`, {
+export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+  logger.info(`[Requested] ${req.method} ${req.originalUrl}`, {
     referrer: req.headers.referer ?? null,
     body: req.body ?? null,
     authorization: req.headers.authorization ?? null,
   });
+  const { send } = res;
+  res.send = function (body) {
+    logger.debug(`[Responded] ${this.req.method} ${this.req.originalUrl}`, {
+      request: {
+        body: this.req.body,
+      },
+      response: {
+        statusCode: this.statusCode,
+        message: this.statusMessage,
+        body,
+      },
+    });
+    return send.call(this, body);
+  }
   next();
 };
