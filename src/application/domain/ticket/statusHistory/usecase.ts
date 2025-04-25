0eb285ade6e6c4a0fb2d6fb0bc49f368138a4582
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-04-25 11:07:55 +0900

    refactor: remove utils of ticket status history

--- src/application/domain/ticket/statusHistory/usecase.ts
+++ src/application/domain/ticket/statusHistory/usecase.ts
@@ -1,25 +1,28 @@
 import {
   GqlQueryTicketStatusHistoriesArgs,
   GqlQueryTicketStatusHistoryArgs,
-  GqlTicketStatusHistory,
   GqlTicketStatusHistoriesConnection,
+  GqlTicketStatusHistory,
 } from "@/types/graphql";
 import { IContext } from "@/types/server";
-import { TicketStatusHistoryUtils } from "@/application/domain/ticket/statusHistory/utils";
 import TicketStatusHistoryService from "@/application/domain/ticket/statusHistory/service";
 import TicketStatusHistoryPresenter from "@/application/domain/ticket/statusHistory/presenter";
+import { clampFirst } from "@/application/domain/utils";
 
 export default class TicketStatusHistoryUseCase {
   static async visitorBrowseTicketStatusHistories(
     ctx: IContext,
     { cursor, filter, sort, first }: GqlQueryTicketStatusHistoriesArgs,
   ): Promise<GqlTicketStatusHistoriesConnection> {
-    return TicketStatusHistoryUtils.fetchTicketStatusHistoriesCommon(ctx, {
-      cursor,
-      filter,
-      sort,
-      first,
-    });
+    const take = clampFirst(first);
+    const res = await TicketStatusHistoryService.fetchTicketStatusHistories(
+      ctx,
+      { cursor, filter, sort },
+      take,
+    );
+    const hasNextPage = res.length > take;
+    const data = res.slice(0, take).map((record) => TicketStatusHistoryPresenter.get(record));
+    return TicketStatusHistoryPresenter.query(data, hasNextPage);
   }
 
   static async visitorViewTicketStatusHistory(
