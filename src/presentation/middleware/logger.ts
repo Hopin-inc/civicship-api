7e8a69cc0adb3a686d0244e1b6cefc77af10c5cb
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2025-04-25 09:01:08 +0900

    feat: modify log level "info" -> "debug"

--- src/presentation/middleware/logger.ts
+++ src/presentation/middleware/logger.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import logger from "@/infrastructure/logging";
 
 export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
-  logger.info(`[Requested] ${req.method} ${req.originalUrl}`, {
+  logger.debug(`[Requested] ${req.method} ${req.originalUrl}`, {
     referrer: req.headers.referer ?? null,
     body: req.body ?? null,
     authorization: req.headers.authorization ?? null,
