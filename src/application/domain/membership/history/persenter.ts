ea3abee9b3f041398042524e8ddb802171a24ef6
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-04-25 11:05:51 +0900

    refactor: rename directory name

--- src/application/domain/membership/history/persenter.ts
+++ src/application/domain/membership/history/persenter.ts
@@ -1,5 +1,5 @@
 import { GqlMembershipHistory } from "@/types/graphql";
-import { PrismaMembershipHistory } from "@/application/domain/membership/membershipHistory/data/type";
+import { PrismaMembershipHistory } from "@/application/domain/membership/history/data/type";
 
 export default class MembershipHistoryPresenter {
   static get(r: PrismaMembershipHistory): GqlMembershipHistory {
