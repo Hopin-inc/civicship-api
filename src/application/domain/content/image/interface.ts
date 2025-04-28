6043f4d1bef78805a1c15c97bf173253a738f564
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-04-28 17:39:11 +0900

    refactor: create service interface

---
+++ src/application/domain/content/image/interface.ts
@@ -0,0 +1,6 @@
+import { GqlImageInput } from "@/types/graphql";
+import { Prisma } from "@prisma/client";
+
+export interface IImageService {
+  uploadPublicImage(image: GqlImageInput, folder: string): Promise<Prisma.ImageCreateWithoutParticipationsInput>;
+} 
