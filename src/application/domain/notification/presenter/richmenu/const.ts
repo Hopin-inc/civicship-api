ed52e42286ac80e87b3cb08091e7e792b28aedc9
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-05-20 15:48:19 +0900

    ``` fix: ensure environment variables and JSON validation in richmenu script
    - Added `envsubst` to embed environment variables into JSON files.
    - Introduced JSON syntax validation using `jq` to prevent errors.
    - Updated rich menu IDs in `const.ts` to align with new configurations.
    ```

--- src/application/domain/notification/presenter/richmenu/const.ts
+++ src/application/domain/notification/presenter/richmenu/const.ts
@@ -1,5 +1,5 @@
 export const LINE_RICHMENU = {
-  ADMIN_MANAGE: 'richmenu-4103686dee2ba894e67e90df71a4a0c4',
-  ADMIN_USER: 'richmenu-7acab3dd947794057453a56b1b83a905',
-  PUBLIC: 'richmenu-a533153c004dbc92b12b339e260eb416',
+  ADMIN_MANAGE: 'richmenu-7d4cf3b69d32f9fb2fc704bd25462695',
+  ADMIN_USER: 'richmenu-580a77998c6752c510831746bb754d31',
+  PUBLIC: 'richmenu-99f8a456c801bbfcdc34be24dd3aa4ee',
 };
