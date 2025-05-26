8f24cb915fe64ace811127ce26c20e84921514c7
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2025-05-27 07:58:02 +0900

    fix: escape rich menu id when undefined

--- src/application/domain/notification/presenter/richmenu/const.ts
+++ src/application/domain/notification/presenter/richmenu/const.ts
@@ -1,5 +1,5 @@
 export const LINE_RICHMENU = {
-  ADMIN_MANAGE: process.env.RICH_MENU_ID_ADMIN_MANAGE,
-  ADMIN_USER: process.env.RICH_MENU_ID_ADMIN_USER,
-  PUBLIC: process.env.RICH_MENU_ID_PUBLIC,
+  ADMIN_MANAGE: process.env.RICH_MENU_ID_ADMIN_MANAGE ?? "",
+  ADMIN_USER: process.env.RICH_MENU_ID_ADMIN_USER ?? "",
+  PUBLIC: process.env.RICH_MENU_ID_PUBLIC ?? "",
 };
