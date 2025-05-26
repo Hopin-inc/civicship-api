df19280ad2ae80fcd61d95dd40b0ee290a2d6d26
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-05-27 04:11:22 +0900

    chore: generate RICHMENU ID

--- src/application/domain/notification/presenter/richmenu/const.ts
+++ src/application/domain/notification/presenter/richmenu/const.ts
@@ -1,5 +1,5 @@
 export const LINE_RICHMENU = {
-  ADMIN_MANAGE: 'richmenu-b572680ec6e251795e61d716385712a4',
-  ADMIN_USER: 'richmenu-7053e48fdbc0cb22c0f73c8ef1eafa2d',
-  PUBLIC: 'richmenu-efcf2aaab28d7704e38c5c00ba43bff1',
+  ADMIN_MANAGE: 'richmenu-148169b8c54e6a092a3ba6c6ac63171f',
+  ADMIN_USER: 'richmenu-e793532438502cb83fa98e54b9ac055a',
+  PUBLIC: 'richmenu-21a7c2de026ea27cbde32924a265be33',
 };
