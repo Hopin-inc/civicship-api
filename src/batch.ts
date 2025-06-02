d4af5c9f908bc590d290cc0fcd932a7b0187f2ba
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-06-02 20:40:08 +0900

    ``` refactor: comment out unused batch processes syncDIDVC and syncWithIdentus
    - Disabled `syncDIDVC` and `syncWithIdentus` to clean up batch process code.
    - Preserved original code by commenting instead of deleting for future reference.
    - Updated `batchProcess` to reflect changes and prevent unwanted calls.
    ```

--- src/batch.ts
+++ src/batch.ts
@@ -1,11 +1,10 @@
 import logger from "@/infrastructure/logging";
 import { resizeImages } from "@/presentation/batch/resizeImages";
-import { syncWithIdentus } from "@/presentation/batch/syncWithIdentus";
 
 export async function batchProcess() {
   switch (process.env.BATCH_PROCESS_NAME) {
     case "sync-did-vc":
-      await syncWithIdentus();
+      // await syncWithIdentus();
       return;
     case "send-line-messages":
       return;
