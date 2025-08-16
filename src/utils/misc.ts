7c92dd8a60dadec68d951b6088a80c1325aa67dc
shinji-nakashima <shinji.nakashima@hopin.co.jp>
2025-08-16 11:30:20 +0900

    feat: add mode and limit in batch request-did-vc

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,10 +1,3 @@
-import { IdentityPlatform } from "@prisma/client";
-
-export const getPlatform = (providerId?: string): IdentityPlatform | undefined => {
-  switch (providerId) {
-    case "oidc.line":
-      return IdentityPlatform.LINE;
-    default:
-      return undefined;
-  }
+export const checkBit = (a: number, n: number): boolean => {
+  return (a & (1 << (n - 1))) !== 0;
 };
