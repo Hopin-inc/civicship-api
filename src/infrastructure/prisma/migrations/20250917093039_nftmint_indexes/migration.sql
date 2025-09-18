0422be0fd006bbda9ae8cd88010403ac1daa0ff8
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-09-18 15:15:23 +0900

    "Remove outdated NFT minting and ordering schema definitions."

--- src/infrastructure/prisma/migrations/20250917093039_nftmint_indexes/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "t_nft_mints_receiver_idx" ON "t_nft_mints"("receiver");
-
--- CreateIndex
-CREATE INDEX "t_nft_mints_created_at_id_idx" ON "t_nft_mints"("created_at", "id");
-
--- CreateIndex
-CREATE INDEX "t_nft_mints_policy_id_created_at_idx" ON "t_nft_mints"("policy_id", "created_at");
--- src/infrastructure/prisma/migrations/20250918054953_unique_wallet_address_of_nft_wallet/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[wallet_address]` on the table `t_nft_wallets` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "t_nft_wallets_wallet_address_key" ON "t_nft_wallets"("wallet_address");
