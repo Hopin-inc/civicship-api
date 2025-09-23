387dc0d18ebfcc5065ab1b69cfa128280a846b6c
宮下裕希 <hirokixyzrr@gmail.com>
2025-09-23 18:41:36 +0900

    不要なimportを削除

--- src/application/domain/reward/ticket/usecase.ts
+++ src/application/domain/reward/ticket/usecase.ts
@@ -27,7 +27,6 @@ import WalletValidator from "@/application/domain/account/wallet/validator";
 import MembershipService from "@/application/domain/account/membership/service";
 import { getCurrentUserId } from "@/application/domain/utils";
 import { ITransactionService } from "@/application/domain/transaction/data/interface";
-import { TransactionReason } from "@prisma/client";
 
 @injectable()
 export default class TicketUseCase {
