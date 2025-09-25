d08c55371649d595d925ffbe24541eb1eddb2d53
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-09-25 13:28:53 +0900

    "Remove unused error handling logic from OrderPresenter."

--- src/application/domain/order/presenter.ts
+++ src/application/domain/order/presenter.ts
@@ -1,14 +1,8 @@
 import { injectable } from "tsyringe";
-import { GqlOrder, GqlOrderCreateError, GqlOrderItem, GqlOrderStatus } from "@/types/graphql";
+import { GqlOrder, GqlOrderItem, GqlOrderStatus } from "@/types/graphql";
 import { OrderWithItems, OrderItemWithProduct } from "./data/type";
 import UserPresenter from "@/application/domain/account/user/presenter";
 import ProductPresenter from "@/application/domain/product/presenter";
-import {
-  InsufficientInventoryError,
-  NmkrApiError,
-  OrderValidationError,
-  ProductNotFoundError,
-} from "@/application/domain/order/errors";
 
 @injectable()
 export default class OrderPresenter {
@@ -37,27 +31,6 @@ export default class OrderPresenter {
       nftMints: [],
       createdAt: item.createdAt,
       updatedAt: item.updatedAt,
-    };
-  }
-
-  static error(error: unknown): GqlOrderCreateError {
-    if (error instanceof InsufficientInventoryError) {
-      return {
-        __typename: "OrderCreateError",
-        message: error.message,
-        code: "INSUFFICIENT_INVENTORY",
-      };
-    }
-    if (error instanceof ProductNotFoundError) {
-      return { __typename: "OrderCreateError", message: error.message, code: "PRODUCT_NOT_FOUND" };
-    }
-    if (error instanceof OrderValidationError || error instanceof NmkrApiError) {
-      return { __typename: "OrderCreateError", message: error.message, code: error.code };
-    }
-    return {
-      __typename: "OrderCreateError",
-      message: "Order creation failed",
-      code: "INTERNAL_ERROR",
     };
   }
 }
