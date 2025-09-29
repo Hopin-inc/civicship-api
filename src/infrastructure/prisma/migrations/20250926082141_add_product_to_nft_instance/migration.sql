33dd5baceaffd6d1965b883008b96c7fd35e2e56
naoki_sakata <naoki.sakata@hopin.co.jp>
2025-09-29 21:25:31 +0900

    "Consolidate and restructure NFT schema with new fields, enums, and constraints."

--- src/infrastructure/prisma/migrations/20250926082141_add_product_to_nft_instance/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "t_nft_instances" ADD COLUMN     "product_id" TEXT;
-
--- AddForeignKey
-ALTER TABLE "t_nft_instances" ADD CONSTRAINT "t_nft_instances_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "t_products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/infrastructure/prisma/migrations/20250927010722_add_nft_wallet_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "t_nft_wallets" ADD COLUMN     "api_key" TEXT,
-ADD COLUMN     "external_ref" TEXT;
