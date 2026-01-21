f3d3c1d969f3f9f34bc7b31795de09327e198cd3
sigma-xing2 <78943811+sigma-xing2@users.noreply.github.com>
2026-01-21 15:55:19 +0900

    fix: Add user and community details to IncentiveGrant presenter and type

--- src/application/domain/transaction/incentiveGrant/data/type.ts
+++ src/application/domain/transaction/incentiveGrant/data/type.ts
@@ -1,4 +1,6 @@
 import { Prisma } from "@prisma/client";
+import { communitySelectDetail } from "@/application/domain/account/community/data/type";
+import { userSelectDetail } from "@/application/domain/account/user/data/type";
 
 export const incentiveGrantSelect = Prisma.validator<Prisma.IncentiveGrantSelect>()({
   id: true,
@@ -6,7 +8,9 @@ export const incentiveGrantSelect = Prisma.validator<Prisma.IncentiveGrantSelect
   sourceId: true,
   status: true,
   userId: true,
+  user: { select: userSelectDetail },
   communityId: true,
+  community: { select: communitySelectDetail },
   failureCode: true,
   lastError: true,
   attemptCount: true,
--- src/application/domain/transaction/incentiveGrant/presenter.ts
+++ src/application/domain/transaction/incentiveGrant/presenter.ts
@@ -1,5 +1,7 @@
 import { PrismaIncentiveGrant } from "./data/type";
 import { GqlIncentiveGrant, GqlIncentiveGrantsConnection } from "@/types/graphql";
+import CommunityPresenter from "@/application/domain/account/community/presenter";
+import UserPresenter from "@/application/domain/account/user/presenter";
 
 export default class IncentiveGrantPresenter {
   static query(r: GqlIncentiveGrant[], totalCount: number, hasNextPage: boolean, cursor?: string): GqlIncentiveGrantsConnection {
@@ -20,9 +22,12 @@ export default class IncentiveGrantPresenter {
   }
 
   static get(r: PrismaIncentiveGrant): GqlIncentiveGrant {
+    const { user, community, ...rest } = r;
     return {
       __typename: "IncentiveGrant",
-      ...r,
+      ...rest,
+      user: UserPresenter.get(user),
+      community: CommunityPresenter.get(community),
     } as GqlIncentiveGrant;
   }
 }
