e45bfce4093da8a29e26fe19d03aee711a7a55d7
naoki_sakata <709sakata@gmail.com>
2026-01-30 19:48:35 +0900

    "Update seed and factory to support Himeji YMCA community configuration."

--- src/infrastructure/prisma/factories/factory.ts
+++ src/infrastructure/prisma/factories/factory.ts
@@ -130,6 +130,26 @@ export const CommunityConfigFactory = defineCommunityConfigFactory.withTransient
 
     return {
       community: { connect: { id: community.id } },
+      portalConfig: {
+        create: {
+          tokenName: "姫路YMCA",
+          title: "姫路YMCA",
+          description: "姫路YMCA",
+          shortDescription: "姫路YMCA",
+          domain: "https://himeji-ymca.civicship.jp",
+          faviconPrefix:
+            "https://storage.googleapis.com/kyoso-dev-civicship-storage-public/communities/himeji-ymca/favicon-32.png",
+          logoPath:
+            "https://storage.googleapis.com/kyoso-dev-civicship-storage-public/communities/himeji-ymca/logo.jpg",
+          squareLogoPath:
+            "https://storage.googleapis.com/kyoso-dev-civicship-storage-public/communities/himeji-ymca/logo-square.jpg",
+          ogImagePath:
+            "https://storage.googleapis.com/prod-civicship-storage-public/asset/himeji-ymca/ogp.jpg",
+          enableFeatures: ["points", "opportunities", "quests"],
+          rootPath: "/users/me",
+          adminRootPath: "/admin/wallet",
+        },
+      },
       firebaseConfig: {
         create: {
           tenantId: process.env.FIREBASE_AUTH_TENANT_ID ?? "",
